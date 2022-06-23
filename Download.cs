000440fb6a3ee4c022863bf6b3e0b8c6414dbffd
starscriptuz <info.enjoymedia@mail.ru>
2022-06-23 22:22:09 +0500

    First commit

---
+++ Download.cs
@@ -0,0 +1,56 @@
+using System.Net;
+using System.Diagnostics;
+
+public class AlQuran
+{
+    private string _curPath = Directory.GetCurrentDirectory();
+    private int _countDo = 0;
+    public delegate void OnDownloadedDelegate(int orderid, Stopwatch x);
+    public OnDownloadedDelegate OnDownloaded {get;set;}
+    public AlQuran()
+    {
+        
+    }
+    private void run(int number, int downloadFileID)
+    {
+        var timer = new Stopwatch();
+        timer.Start();
+        using (var client = new WebClient())
+        {
+            client.DownloadFile($"https://ia803406.us.archive.org/27/items/AlQuranWithEnglishSaheehIntlTranslation--RecitationByMishariIbnRashidAl-AfasyWithIbrahimWalk/{number:D3}.mp3", $"{_curPath}/downloads/{number:D3}.mp3");
+            OnDownloaded?.Invoke(downloadFileID,timer);
+        }
+    }
+
+    public async void Download(int number)
+    {
+        if(1 <= number && number <= 114)
+        {
+            _countDo++;
+            Console.WriteLine($"#{_countDo}: Download started");
+            
+            await Task.Run(() => run(number,_countDo));        
+        }
+        else
+        {
+            ColorPrint("ERROR !!! : The surah you entered does not exist in our base!",_Colors.Red);
+        }
+    }
+
+    public enum _Colors
+    {
+        Red,
+        Green,
+        White
+    }
+    public void ColorPrint(string text,_Colors color)
+    {
+        var rang = color switch
+        {
+            _Colors.Red => Console.ForegroundColor = ConsoleColor.Red,
+            _Colors.Green => Console.ForegroundColor = ConsoleColor.Green
+        };
+        Console.WriteLine($"{text}",Console.ForegroundColor);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+var obj = new AlQuran();
+obj.OnDownloaded += HandleDownloaded;
+
+void HandleDownloaded(int ID,Stopwatch x)
+{
+    obj.ColorPrint($"  #{ID}- Download file has been finished in ({x.ElapsedMilliseconds / 1000}sec or {x.ElapsedMilliseconds}ms).",AlQuran._Colors.Green);    
+}
+while (true)
+{
+    var x = int.Parse(Console.ReadLine());
+    obj.Download(x);
+}
